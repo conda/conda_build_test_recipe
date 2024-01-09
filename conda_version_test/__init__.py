92c836a7cc2362122dc0900290983024a32d28b8
Ken Odegard <kodegard@anaconda.com>
2024-01-09 14:34:33 -0600

    Update versioneer (#6)

--- conda_version_test/__init__.py
+++ conda_version_test/__init__.py
@@ -1,9 +1,6 @@
 # -*- coding: utf-8 -*-
+from . import _version
 
 __author__ = 'Continuum Analytics'
 __email__ = 'conda@continuum.io'
-__version__ = '0.1.0_1'
-
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
+__version__ = _version.get_versions()['version']
