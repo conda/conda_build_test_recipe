a84c76a96f5518748683956cfbb55b2c8e7f464c
Ken Odegard <kodegard@anaconda.com>
2024-01-09 14:33:56 -0600

    Cleanup

--- conda_version_test/__init__.py
+++ conda_version_test/__init__.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
+from . import _version
 
 __author__ = 'Continuum Analytics'
 __email__ = 'conda@continuum.io'
-__version__ = '0.1.0_1'
-
-from . import _version
 __version__ = _version.get_versions()['version']
